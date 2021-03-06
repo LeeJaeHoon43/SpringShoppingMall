283dc65da2e4a9018766eec5265988bb2c9ebe14
leejaehoon43 <ljw160791@gmail.com>
2021-03-06 23:19:10 +0900

    1. 이메일 전송 환경 세팅 2. 인증번호 입력란 색상 변경 및 disabled 속성 추가 3. 이메일 전송 메서드 추가(자바스크립트) 4. Controller 메서드 추가 1. 인증번호(난수) 생성 2. 인증번호 이메일 전송 3. 인증번호 뷰(회원가입 페이지)로 전송 1. 인증번호 입력란 변환 기능 2. 인증번호 일치여부 검사 기능 1. 외부 스크립트 파일 연결 코드 추가 2. 주소 검색 기능 메서드 메서드 추가 3. 다음 주소 팝업창 코드 추가 4. [주소찾기]버튼과 메서드 연결 5. 테스트 6. 주소 입력 예제 코드 추가 7. 예제코드 수정

--- src/main/java/com/ljh/controller/MemberController.java
+++ src/main/java/com/ljh/controller/MemberController.java
@@ -1,14 +1,16 @@
 package com.ljh.controller;
 
+import java.util.Random;
+import javax.mail.internet.MimeMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import com.ljh.model.MemberVO;
 import com.ljh.service.MemberService;
 
@@ -19,10 +21,12 @@ public class MemberController {
 	private static final Logger logger = LoggerFactory.getLogger(MemberController.class);
 	
 	private MemberService memberService;
+	private JavaMailSender mailSender;
 	
 	@Autowired
-	public MemberController(MemberService memberService) {
+	public MemberController(MemberService memberService, JavaMailSender mailSender) {
 		this.memberService = memberService;
+		this.mailSender = mailSender;
 	}
 	
 	@RequestMapping(value = "/join", method = RequestMethod.GET)
@@ -53,5 +57,39 @@ public class MemberController {
 		}else {
 			return "success"; // 중복 아이디가 없음.
 		}
+	}
+	
+	@RequestMapping(value = "/mailCheck", method = RequestMethod.GET)
+	@ResponseBody
+	public String mailCheckGET(String email) throws Exception{
+		// 인증번호 난수 생성.
+		Random random = new Random();
+		int checkNum = random.nextInt(888888) + 111111;
+		logger.info("인증번호 : " + checkNum);
+		
+		// 이메일로 인증번호 보내기.
+		String setFrom = "ljh53502@naver.com";
+		String toMail = email;
+		String title = "회원가입 인증메일입니다.";
+		String content = 
+				"홈페이지를 방문해주셔서 감사합니다." +
+				"<br><br>" + 
+				"인증 번호는 " + checkNum + "입니다." + 
+				"<br>" + 
+				"해당 인증번호를 인증번호 확인란에 기입하여 주세요.";
+		try {
+			MimeMessage message = mailSender.createMimeMessage();
+			MimeMessageHelper helper = new MimeMessageHelper(message, true, "utf-8");
+			helper.setFrom(setFrom);
+			helper.setTo(toMail);
+			helper.setSubject(title);
+			helper.setText(content, true);
+			mailSender.send(message);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		String num = Integer.toString(checkNum);
+		return num;
 	}
 }
